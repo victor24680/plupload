2324305b344d6a1a4ca091eba58a6804fd63bfe6
victor24680 <490319148@qq.com>
2017-03-26 12:01:16 +0800

    commit mytq

---
+++ Webs/Home/Controller/IndexController.class.php
@@ -0,0 +1,132 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    // public function index(){
+    //     $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    // }
+    public function index(){
+    	$this->display();
+    }
+
+
+    public function upload(){
+    	F('param',$_REQUEST['param']);
+    	F('FILE',$_FILES);
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+		header("Cache-Control: no-store, no-cache, must-revalidate");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+
+		/* 
+		// Support CORS
+		header("Access-Control-Allow-Origin: *");
+		// other CORS headers if any...
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+			exit; // finish preflight CORS requests here
+		}
+		*/
+
+		// 5 minutes execution time
+		@set_time_limit(5 * 60);
+
+		// Uncomment this one to fake upload time
+		// usleep(5000);
+
+		// Settings-------Uploads/
+		$targetDir ='Uploads/';//ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
+
+		//$targetDir = 'uploads';
+		$cleanupTargetDir = true; // Remove old files
+		$maxFileAge = 5 * 3600; // Temp file age in seconds
+
+
+		// Create target dir
+		if (!file_exists($targetDir)) {
+			@mkdir($targetDir);
+		}
+
+		// Get a file name
+		if (isset($_REQUEST["name"])) {
+			$fileName = $_REQUEST["name"];
+		} elseif (!empty($_FILES)) {
+			$fileName = $_FILES["file"]["name"];
+		} else {
+			$fileName = time().'.jpg';//uniqid("file_");
+		}
+		//重命名文件
+		$fileReName=explode('.',$fileName);
+		$fileReName[0]=date("YmdHis",time()).rand(1000,9999);
+		$fileName=implode('.',$fileReName);
+
+		$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+	
+		// Chunking might be enabled【块可能被启用】
+		$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+		$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+
+
+		// Remove old temp files，删除旧的临时文件
+		if ($cleanupTargetDir) {
+			if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
+				die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+			}
+
+			while (($file = readdir($dir)) !== false) {
+				$tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+				// If temp file is current file proceed to the next【如果临时文件是当前文件，则转到下一个文件】
+				if ($tmpfilePath == "{$filePath}.part") {
+					continue;
+				}
+
+				//删除温度文件，如果它是大于最大年龄，而不是当前文件
+				// Remove temp file if it is older than the max age and is not the current file
+				if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+					@unlink($tmpfilePath);
+				}
+			}
+			closedir($dir);
+		}	
+
+
+		// Open temp file【打开临时文件】
+		if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
+			die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+		}
+
+		if (!empty($_FILES)) {
+			if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+				die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+			}
+
+			// Read binary input stream and append it to temp file
+			if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+			}
+		} else {	
+			if (!$in = @fopen("php://input", "rb")) {
+				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+			}
+		}
+
+		while ($buff = fread($in, 4096)) {
+			fwrite($out, $buff);
+		}
+
+		@fclose($out);
+		@fclose($in);
+
+		// Check if file has been uploaded
+		if (!$chunks || $chunk == $chunks - 1) {
+			// Strip the temp .part suffix off 
+			rename("{$filePath}.part", $filePath);
+			echo '200-OK';
+		}
+
+		// Return Success JSON-RPC response
+		//die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+	}
+
+}
